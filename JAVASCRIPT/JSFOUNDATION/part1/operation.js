e465cde925273215cdf835706a9400d6f95959f4
shubh sahu <shubhsahu2006@gmail.com>
2026-09-15 00:59:43 +0530

    Add JavaScript arithmetic operations example

---
+++ JAVASCRIPT/JSFOUNDATION/part1/operation.js
@@ -0,0 +1,39 @@
+
+let score=102;
+let bonus=25;
+
+let totalScore=score+bonus;
+
+let addition=4+5;
+let substract=9-3;
+let mult=3*5;
+let divi=8/2;
+let remainder=9%2;
+let exponent=2**4;
+
+let myscore=110;
+myscore++;
+++myscore;
+
+console.log(myscore);
+
+let credits=56;
+credits--;
+console.log(credits);
+
+//comparison operators
+
+let num1=3;
+let num2=3;
+let num3=6;
+
+console.log(num1==num2);//true
+console.log(num1==num3);//false
+console.log(num1!=num3);//true
+console.log(num1<num3);//true
+console.log(num1>num3);//false
+
+
+
+
+
