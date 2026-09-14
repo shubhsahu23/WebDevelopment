13a0fdc5fba0dde505ebc5ac0d9b80137c88b871
shubh sahu <shubhsahu2006@gmail.com>
2026-09-15 00:59:19 +0530

    Add JavaScript data types example

---
+++ JAVASCRIPT/JSFOUNDATION/part1/datatype.js
@@ -0,0 +1,26 @@
+/*
+String
+Number
+Boolean
+BigInt
+
+undefined
+null
+
+Object
+
+Symbol
+
+*/
+
+let score=102;
+let name="Shubh";
+let isLoggedin=false;
+
+//object
+let teatypes=["lemon tea","orange tea", "black tea"];
+let user={name:"shubh",age:20};
+
+let getScore=score;
+
+console.log(getScore);
