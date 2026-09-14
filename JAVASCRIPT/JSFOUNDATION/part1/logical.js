0541e02652172fc5fa67c2ff42981d94b59ee535
shubh sahu <shubhsahu2006@gmail.com>
2026-09-15 00:59:25 +0530

    Add JavaScript logical operators example

---
+++ JAVASCRIPT/JSFOUNDATION/part1/logical.js
@@ -0,0 +1,14 @@
+// && -> and
+// || -> or
+// ! -> not(reverse)
+
+let isLoggedin=true;
+let isPaid=false;
+
+console.log(isLoggedin && isPaid);//false
+
+let isEmailUser=true;
+let isGoogleUser=false;
+
+console.log(isEmailUser||isGoogleUser);//true
+
