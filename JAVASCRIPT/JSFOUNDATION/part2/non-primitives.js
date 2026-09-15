339b3727f0747f8da01b9643019d55cf76cdcbb8
shubh sahu <shubhsahu2006@gmail.com>
2026-09-15 11:09:41 +0530

    Add JavaScript non-primitive types examples

---
+++ JAVASCRIPT/JSFOUNDATION/part2/non-primitives.js
@@ -0,0 +1,30 @@
+const obj={
+  name:"shubh",
+  age:20,
+}
+
+obj.name="s";
+obj.lastname="sahu";
+console.log(obj.name);
+console.log(obj["lastname"]);
+console.log(obj);//{}
+console.log(typeof obj);//object
+
+let date=new Date();
+console.log(date);
+
+//Array
+let heros=["ironman","thor","hulk",true,50];
+console.log(heros);
+console.log(heros[0]);
+
+let value=true;
+console.log(value+1);
+console.log("1"+1);
+console.log(1+"1");
+
+
+value="2abc";
+console.log(Number(value) )
+console.log(Number(undefined) )
+
