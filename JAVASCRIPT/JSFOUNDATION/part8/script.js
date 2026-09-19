eedddb1cbeb4d018da95d17cd9b3565899d085ff
shubh sahu <shubhsahu2006@gmail.com>
2026-09-19 12:55:55 +0530

    Add more DOM event examples

--- JAVASCRIPT/JSFOUNDATION/part8/script.js
+++ JAVASCRIPT/JSFOUNDATION/part8/script.js
@@ -38,3 +38,45 @@ addEventListener('click',function(){
   let list=document.getElementById('taskList');
   list.lastElementChild.remove();
 })
+
+//Example 6
+document.getElementById('clickMeButton').
+addEventListener('mouseover',function(){
+  alert("button clicked");
+})
+
+//Example 7
+document.getElementById('teaList').
+addEventListener('click',function(event){
+  if(event.target && event.target.matches('.teaItem')){
+    alert("you selected "+ event.target.textContent);
+  }
+})
+
+//Example 8
+
+document.getElementById('feedbackForm').
+addEventListener('submit',function(e){
+  e.preventDefault();
+  let text=document.getElementById('feedbackInput').value;
+  document.getElementById('feedbackDisplay').textContent=text;
+})
+
+//Example 9
+
+document.addEventListener('DOMContentLoaded',function(){
+  document.getElementById('domStatus').textContent="dom loaded";
+})
+
+//Example 10
+
+document.getElementById('toggleHighlight').
+addEventListener('click',function(){
+  // if(document.getElementById('descriptionText').classList.contains('highlight')){
+  //   document.getElementById('descriptionText').classList.remove('highlight');
+  // }
+  // else{
+  //   document.getElementById('descriptionText').classList.add('highlight');
+  // }
+  document.getElementById('descriptionText').classList.toggle('highlight');
+})
