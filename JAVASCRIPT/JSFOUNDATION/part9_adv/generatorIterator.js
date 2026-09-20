27c6f88519b4f72ab32f23382ca496129bdc6f2e
shubh sahu <shubhsahu2006@gmail.com>
2026-09-20 18:22:48 +0530

    Add JavaScript generator iterator example

---
+++ JAVASCRIPT/JSFOUNDATION/part9_adv/generatorIterator.js
@@ -0,0 +1,12 @@
+function* numberGenerator(){
+  yield 1
+  yield 2
+  yield 3
+}
+
+let gen=numberGenerator();
+
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
+console.log(gen.next().value);
