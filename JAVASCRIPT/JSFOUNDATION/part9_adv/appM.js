6d0e76d67507a4128402e7bb02198f4b6964b49b
shubh sahu <shubhsahu2006@gmail.com>
2026-09-20 18:22:26 +0530

    Add ES module import example

---
+++ JAVASCRIPT/JSFOUNDATION/part9_adv/appM.js
@@ -0,0 +1,8 @@
+//default import
+import multiply from "./mathoperation.js";
+
+console.log(multiply(2,2));
+
+//named import
+import{add,subtract} from "./mathoperation.js";
+console.log(add(2,2));
